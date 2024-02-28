2fb423680b81a277f8cbe38ac14b341c04f09376
Sergey Protopopov <protopopov_s@mail.ru>
2024-02-28 21:00:32 +0300

    Lection 01

--- lections/lection01/main.py
+++ lections/lection01/main.py
@@ -1,1 +1,19 @@
-print("Hello world")
+a = 5
+b = 5.87
+c = 'hello'
+
+print(f"{a} - {b} - {c}")
+print("{} - {} - {}".format(a, b, c))
+
+print("please enter number 1: ")
+# user_input = int(input())
+# user_input2 = int(input("please enter number 2: "))
+
+# print(user_input, user_input2)
+# print(user_input + user_input2)
+
+a = 4.7657677
+b = 3.968785
+
+print(a * b)
+print(round((a * b), 2))
