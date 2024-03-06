dfeb29e79378bada7010710299694d81f1ba0c0d
Sergey Protopopov <protopopov_s@mail.ru>
2024-03-06 20:41:45 +0300

    lection 02

---
+++ lections/lection02/main.py
@@ -0,0 +1,36 @@
+# list_1 = []
+# list_1 = list()
+# list_1 = [1, 3, 6, 8]
+#
+# print(list_1)
+# print(*list_1)
+#
+# print(len(list_1))
+# print(list_1[0])
+# list_1.append(10)
+# print(list_1)
+
+# list_1 = []
+#
+# for i in range(10):
+#     list_1.append(i)
+#     print(list_1)
+#
+# list_1.pop()
+# print(list_1)
+# list_1.pop(0)
+# print(list_1)
+# list_1.pop(0)
+# print(list_1)
+# list_1.insert(2, 11)
+# print(list_1)
+# print(list_1[0])
+# print(list_1[1:3])
+# print(list_1[:])
+
+t = () # пустой кортеж
+
+dict = {} # пустой словарь
+dict['q'] = 'qwerty'
+
+print(dict)
