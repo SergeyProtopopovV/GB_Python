a50064a48c9b3786a64b22259b8f24e8af9fc83f
SergeyProtopopovV <153833663+SergeyProtopopovV@users.noreply.github.com>
2024-03-12 17:38:27 +0300

    Update task2.py

--- seminars/seminar04/task2.py
+++ seminars/seminar04/task2.py
@@ -5,3 +5,16 @@
 # shells on the sea shore I'm sure that the shells are sea shore shells
 
 # Output: 13
+
+input_text = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea shells on the sea shore I'm sure that the shells are sea shore shells"
+
+separated_text = input_text.lower().split()
+
+word_count = {}
+for word in separated_text:
+  if word in word_count:
+    word_count[word] += 1
+  else:
+    word_count[word] = 1
+
+print(len(word_count))
