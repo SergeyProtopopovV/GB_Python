96029a969f809e3e04dbfc48842bb8192f9f4216
Sergey Protopopov <protopopov_s@mail.ru>
2024-03-23 19:27:33 +0300

    homework 07 updated

---
+++ homeworks/homework07/proba.py
@@ -0,0 +1,9 @@
+VOWELS = 'аеёиоуэюяы'
+stroka = 'ПАра-ра-рам рам-пам-папАМ па-ра-пИ-дам'
+
+stroka_vowels = "".join([char for char in stroka.lower() if char in VOWELS or char == ' '])
+if len(stroka_vowels) <= 1:
+    print("Количество фраз должно быть больше одной!")
+else:
+    result = [len(word) for word in stroka_vowels]
+    print("Парам пам-пам") if len(set(result)) == 1 else print("Пам парам")
